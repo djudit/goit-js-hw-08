01bcb5f9ed3c724fb3b1aa8079660af13785e8fc
Yuliia <djudit@ukr.net>
2023-11-19 00:51:26 +0200

    update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,5 @@
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
@@ -21,6 +22,7 @@ function createGallery(pictures) {
 
 const itemPicture = createGallery(galleryItems);
 gallery.insertAdjacentHTML('beforeend', itemPicture);
+gallery.style.listStyle = 'none';
 
 let gallerySimple = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
