2b546860939cff2f3f94aa218b8f29d9dbc4e9a6
Yuliia <djudit@ukr.net>
2023-11-20 18:29:55 +0200

    task 2 localstorage is done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,34 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 
 const player = new Vimeo.Player(iframe);
 console.log(player);
+console.dir(iframe);
 
 // iframe.addEventListener('timeupdate', onPlay);
 
 const onPlay = function (data) {
-  duration: 61.857;
-  percent: 0.049;
-  seconds: 3.034;
+  localStorage.setItem('videoplayer-current-time', data.seconds);
 };
-localStorage.setItem('videoplayer-current-time', onPlay);
 player.on('timeupdate', onPlay);
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+player.on('timeupdate', throttle(storageFunc, 1000));
