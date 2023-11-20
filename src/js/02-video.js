c50e6d415b310f46b109558164ee6956a4f0ed22
Yuliia <djudit@ukr.net>
2023-11-20 02:35:32 +0200

    task 2 startad

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Vimeo.Player(iframe);
+console.log(player);
+
+// iframe.addEventListener('timeupdate', onPlay);
+
+const onPlay = function (data) {
+  duration: 61.857;
+  percent: 0.049;
+  seconds: 3.034;
+};
+localStorage.setItem('videoplayer-current-time', onPlay);
+player.on('timeupdate', onPlay);
